f75fe675122b33d96c424a7c9c751d23fe8dc9a7
MdNadimUddin01 <mdnadimuddin00786@gmail.com>
2024-09-16 00:04:04 +0530

    update 02

---
+++ 01_Basics/03_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+1)  // we are using nodejs , not browser
+
+console.log(3
+    +
+    3
+)//not good practice
+
+//tc39 or MDN -> use for js documentation
+
+let name = "Nadim"
+let age = 19
+let isLogin = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value // representation of empty value
+// undefined =>  // when value is not defined
+// symbol => use for uniqueness
+
+
+
+// object
+
+console.log(typeof null) // object -> interview question
+console.log(typeof "Nadim") // String
+console.log(typeof undefined) // undefined
+
+
+
+
