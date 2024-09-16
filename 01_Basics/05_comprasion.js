e05b43899f4c62ce69b5d8854d8e6676dc07efeb
MdNadimUddin01 <mdnadimuddin00786@gmail.com>
2024-09-16 09:44:01 +0530

    comparison update

---
+++ 01_Basics/05_comprasion.js
@@ -0,0 +1,23 @@
+console.log("02" > 1) // true
+console.log("2" > 1) //true
+
+
+//when you compare anything then make sure the both have same data type
+
+console.log(null > 0) //
+console.log(null == 0) // false
+console.log(null >= 0) // true
+//Bad Practice
+
+//in comparison null = 0
+
+
+console.log(undefined == 0) //false
+console.log(undefined > 0) // false
+console.log(undefined >= 0) // false
+//Bad Practice
+
+// === ->  string check 
+
+console.log("2" == 2) //true
+console.log("2" === 2) //false
